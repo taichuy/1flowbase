617c42f1b2a53f7bc6f4ca6401b3b27a2a4597b4
JAVA-Lw <1264129499@qq.com>
2026-09-09 18:07:12 +0800

    fix(interfaces): anchor nested model projection source paths

--- api/apps/api-server/src/routes/plugins_and_models/model_definitions_interface/managed_projection.rs
+++ api/apps/api-server/src/routes/plugins_and_models/model_definitions_interface/managed_projection.rs
@@ -1,4 +1,6 @@
 use super::*;
 
+#[path = "managed_projection/model_definitions_input.rs"]
 mod model_definitions_input;
+#[path = "managed_projection/model_definitions_output.rs"]
 mod model_definitions_output;
