3450321dd574d85d982d888ddf7852e40d28637d
JAVA-Lw <1264129499@qq.com>
2026-09-22 00:19:27 +0800

    fix(logs): restore run detail conversation composer

--- web/app/src/features/applications/components/logs/ApplicationRunDetailPanel.tsx
+++ web/app/src/features/applications/components/logs/ApplicationRunDetailPanel.tsx
@@ -602,7 +602,7 @@ function RunConversation({
         messages={messages}
         runContext={runConversationContext}
         showClearAction={false}
-        showComposer={false}
+        showComposer
         status={conversationSessionStatus(conversationItems, outputState)}
         stopping={false}
         subtitle={<RunIdSubtitle runId={runId} />}
